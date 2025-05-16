9a0063801c9c5b1cab9e1162881509e13c5493d9
Gittu2raj11 <152615267+Gittu2raj11@users.noreply.github.com>
2025-05-16 19:44:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ API_ID = int(os.environ.get("API_ID", ""))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7216155592:AAEK_pmwjuehRju7-FjBGxTw7G1GD-QAJYQ")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("")
+BOT_USERNAME = os.environ.get("cobra_txtbot")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1843979302").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002650028673"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srvter")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://namanjain123eudhc:opmaster@cluster0.5iokvxo.mongodb.net/?retryWrites=true&w=majority")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-100"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002650028673"))
 
